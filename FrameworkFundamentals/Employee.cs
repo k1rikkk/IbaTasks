b47e38044301e5e9ab9b06a4c9a02f85a7269a10
Kirill <k1rikkk@mail.ru>
2018-03-19 21:23:10 +0300

    Task: Framework Fundamentals

---
+++ FrameworkFundamentals/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkFundamentals
+{
+    public class Employee : IComparable<Employee>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal Salary { get; set; }
+
+        public int CompareTo(Employee other) => Salary.CompareTo(other.Salary);
+    }
+}
---
+++ Tests/FrameworkFundamentalsTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using FrameworkFundamentals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class FrameworkFundamentalsTest
+    {
+        [TestMethod]
+        public void ListEmployeesSort_Randoms_BySalary()
+        {
+            List<Employee> employees = new List<Employee>();
+            int count = 5;
+            for (int i = 0; i < count; i++)
+                employees.Add(new Employee
+                {
+                    FirstName = i.ToString(),
+                    Salary = new Random().Next(0, 200000)
+                });
+            employees.Sort();
+            bool ok = true;
+            for (int i = 1; i < count; i++)
+                if (employees[i].Salary < employees[i - 1].Salary)
+                {
+                    ok = false;
+                    break;
+                }
+            Assert.IsTrue(ok);
+        }
+
+        [TestMethod]
+        public void ListEmployeesSort_BySalary()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { FirstName = "1", Salary = 150000 },
+                new Employee { FirstName = "2", Salary = 200000 },
+                new Employee { FirstName = "3", Salary = 100000 }
+            };
+            employees.Sort();
+            Assert.IsTrue(employees[0].Salary == 100000 && employees[1].Salary == 150000 && employees[2].Salary == 200000);
+        }
+    }
+}

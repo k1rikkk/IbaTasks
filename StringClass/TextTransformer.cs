e548d52f9ddc621cdf6809c7d92d1b2018f6a8a0
Kirill <k1rikkk@mail.ru>
2018-03-26 11:55:56 +0300

    Task: String class

---
+++ StringClass/TextTransformer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringClass
+{
+    public class TextTransformer
+    {
+        public const string Null = "UNSET";
+        protected string val;
+
+        public TextTransformer(string s) => val = s;
+
+        public string Process()
+        {
+            if (string.IsNullOrEmpty(val))
+                return Null;
+            string result = "";
+            foreach (char c in val)
+                result += char.ToUpper(c);
+            return result;
+        }
+    }
+}
---
+++ Tests/StringClassTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringClass;
+
+namespace Tests
+{
+    [TestClass]
+    public class StringClassTest
+    {
+        [TestMethod]
+        public void TextTransformer_Process()
+        {
+            TextTransformer tt = new TextTransformer("Hello, world!");
+            Assert.AreEqual("HELLO, WORLD!", tt.Process());
+        }
+
+        [TestMethod]
+        public void TextTransformer_Process_Null()
+        {
+            Assert.AreEqual(TextTransformer.Null, new TextTransformer("").Process());
+            Assert.AreEqual(TextTransformer.Null, new TextTransformer(null).Process());
+        }
+    }
+}
